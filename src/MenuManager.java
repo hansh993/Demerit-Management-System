a0b81fe067660f8cc9e660c8bd0dc53bfe879078
hansh993 <gkstjdgur52@naver.com>
2022-03-19 20:43:44 +0900

    added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Person");
+		System.out.println("2. Delete Person");
+		System.out.println("3. Deduct Points");
+		System.out.println("4. View Demerit");
+		System.out.println("5. Exist");
+
+	}
+
+}
