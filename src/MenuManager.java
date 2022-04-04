5dec083159360352f5815b4ae4683465694efade
hansh993 <gkstjdgur52@naver.com>
2022-04-04 20:36:07 +0900

    added person.java, personmanager.java and edit menumanager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,61 +3,36 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num=0;
 		Scanner input=new Scanner(System.in);
+		PersonManager personmanager=new PersonManager(input);
 		
-		while(num!=6) {
+		int num=0;
+		
+		while(num!=5) {
 			System.out.println("*** Demerit Management System ***");
 			System.out.println("1. Add Person");
 			System.out.println("2. Delete Person");
 			System.out.println("3. Deduct Points");
 			System.out.println("4. View Demerit");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exist");
-			System.out.print("Select one number between 1-6:");
+			System.out.println("5. Exit");
+			System.out.print("Select one number between 1-5:");
 			num=input.nextInt();
 			switch(num) {
 			case 1:
-				addPerson();
+				personmanager.addPerson();
 				break;
 			case 2:
-				deletePerson();
+				personmanager.deletePerson();
 				break;
 			case 3:
-				deductPoints();
+				personmanager.deductPoints();
 				break;
 			case 4:
-				viewDemerit();
+				personmanager.viewDemerit();
 				break;
 			}	
 			
 		}
 	}
-	public static void addPerson() {
-		Scanner input=new Scanner(System.in);
-		System.out.print("Person's Id:");
-		int personId=input.nextInt();
-		System.out.print("Person's Name:");
-		String Name=input.next();
-		System.out.print("Person's P_no:");
-		int P_no=input.nextInt();
-		System.out.print("Person's Email:");
-		String email=input.next();
-	}
-	public static void deletePerson() {
-		Scanner input=new Scanner(System.in);
-		System.out.print("Person's Id:");
-		int personId=input.nextInt();
-	}
-    public static void deductPoints() {
-    	Scanner input=new Scanner(System.in);
-		System.out.print("Person's Id:");
-		int personId=input.nextInt();
-	}
-    public static void viewDemerit() {
-    	Scanner input=new Scanner(System.in);
-		System.out.print("Person's Id:");
-		int personId=input.nextInt();
-    }
 
 }
