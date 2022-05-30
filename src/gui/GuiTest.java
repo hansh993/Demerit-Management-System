72b0e740163e085c6940232bc3253d714bd038c5
hansh993 <gkstjdgur52@naver.com>
2022-05-30 22:17:33 +0900

    GUI

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+		//MenuSelection menuselection = new MenuSelection();
+		//PersonAdder personadder = new PersonAdder();
+		PersonViewer personviewer = new PersonViewer();
+	}
+
+}
---
+++ src/gui/PersonAdder.java
@@ -0,0 +1,52 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class PersonAdder extends JFrame {
+	public PersonAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelID = new JLabel("ID: ", JLabel.TRAILING);
+		JTextField fieldID = new JTextField(10);
+		labelID.setLabelFor(fieldID);
+		panel.add(labelID);
+		panel.add(fieldID);
+		
+		JLabel labelName = new JLabel("Name: ", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelName.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		JLabel labelEmail = new JLabel("Email: ", JLabel.TRAILING);
+		JTextField fieldEmail = new JTextField(10);
+		labelEmail.setLabelFor(fieldEmail);
+		panel.add(labelEmail);
+		panel.add(fieldEmail);
+		
+		JLabel labelP_no = new JLabel("P_no: ", JLabel.TRAILING);
+		JTextField fieldP_no = new JTextField(10);
+		labelP_no.setLabelFor(fieldP_no);
+		panel.add(labelP_no);
+		panel.add(fieldP_no);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancel"));
+		
+		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6);
+		
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(panel);
+		this.setVisible(true);
+		
+		
+	}
+
+}
